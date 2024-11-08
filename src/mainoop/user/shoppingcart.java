54bbe011c02ced420b27fe93af0b64b1b98b7a03
NhatNguyen <nhatnguyen27042005@gmail.com>
2024-11-08 15:26:50 +0700

    Nguyen 5

--- src/mainoop/user/shoppingcart.java
+++ src/mainoop/user/shoppingcart.java
@@ -1,5 +1,21 @@
 package mainoop.user;
 
-public class shoppingcart {
+import java.util.ArrayList;
+import mainoop.product.Product;
+
+public class ShoppingCart {
+    private int customerId;
+    private ArrayList<Product> listProductShoppingCart = new ArrayList<>();
+    private ArrayList listQuantityProductShoppingCart = new ArrayList<>();
+    private int sumPriceProduct;
+
+    public ShoppingCart() {}
     
+    public ShoppingCart(int customerId, ArrayList<Product> listProductShoppingCart, ArrayList listQuantityProductShoppingCart, int sumPriceProduct) {
+        this.customerId = customerId;
+        this.listProductShoppingCart = listProductShoppingCart;
+        this.listQuantityProductShoppingCart = listQuantityProductShoppingCart;
+        this.sumPriceProduct = sumPriceProduct;
+    }
+
 }
