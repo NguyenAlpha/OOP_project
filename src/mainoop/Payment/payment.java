687201b42f848360bdce38145742b3851765135e
NhatNguyen <nhatnguyen27042005@gmail.com>
2024-11-29 15:56:22 +0700

    fix

--- src/mainoop/Payment/payment.java
+++ src/mainoop/Payment/payment.java
@@ -37,7 +37,6 @@ public class payment {
         return this.admin;
     }
 
-
     // ------------------ Setter ---------------------
     public void setCustomer(Customer customer) {
         this.customer = customer;
@@ -47,35 +46,6 @@ public class payment {
     }
     public void setpaymentID(int paymentID) {
         this.paymentID =  currentID++;
-    }
-
-    // --------- Hóa Đơn ----------
-    public void bill() {
-        // if (customer == null) {
-        //     System.out.println("Không có thông tin khách hàng!");
-        //     return;
-        // }
-
-        
-        System.out.println("Xin chào, " + customer.getCustomerName());
-        System.out.println("========================= Hoa Don =====================");
-        System.out.println("Mã Đơn hàng: " + getpaymentID());
-        System.out.println("Tên khách hàng: " + customer.getCustomerName());
-        System.out.println("Danh sách sản phẩm:");
-        System.out.printf("%-20s | %-10s | %-15s | %-15s\n", "Tên hàng", "SL", "Đơn giá", "Thành tiền");
-
-        for (Map.Entry<Product, Integer> entry : customer.getCartItem().entrySet()) {
-            Product product = entry.getKey();
-            int quantity = entry.getValue();
-            long productTotal = product.getProductPrice() * quantity;
-
-            // Hiển thị chi tiết sản phẩm
-            System.out.printf("%-20s | %-10d | %-15d | %-15d\n",
-                    product.getProductName(), quantity, product.getProductPrice(), productTotal);
-        }
-
-        System.out.println("------------------------------------------------------");
-        System.out.println("Tong cong: " + customer.getSumPriceProduct() + " VND");
     }
 
     public Map<Product, Integer> getProductInBill() {
